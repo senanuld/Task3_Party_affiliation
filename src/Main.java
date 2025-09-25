63bb6a42429594ffb5219b1738aaff8b84f28e87
Leonard Senanu <senanuld@mail.uc.edu>
2025-09-24 20:36:26 -0400

    Task4 updated

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //cascade if then els
+        Scanner input = new Scanner(System.in);
+        String menuChoice = "";
+        System.out.print("Enter party affiliation: ");
+        input = new Scanner(System.in);
+        switch (input.next()) {
+        }
+        System.out.print("you get a Democratic Donkey: ");
+        System.out.print("you get a Republican Donkey: ");
+        System.out.print("you get an independent person: ");
+        System.out.print("you get other: ");
+    }
+}
